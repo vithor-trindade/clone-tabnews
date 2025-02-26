b39527e1de5f9d8a5b5ad81ffad12521de6db803
vithor-trindade <trindadevithor@gmail.com>
2025-02-26 01:10:17 +0000

    adiciona o arquivo index.js modificado

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-function Home() { return <h1>Teste 3</h1> ; }
+function Home() { return <h1>Meu amor, obrigado por existir na minha vida. Eu te amo todos os dias pra sempre❤️</h1> }
 
 export default Home;
